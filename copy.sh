919735552670abda32c042af0c9edf9fe5921f8b
Mikhail Troshev <mishanga@yandex-team.ru>
2013-02-01 17:26:20 +0400

    Forsed scp

--- copy.sh
+++ copy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 scp -r .ssh $1:~/
 scp -r cfgs $1:~/
-ssh $1 'cd && ln -s cfgs/.vim .'
-ssh $1 'cd && ln -s cfgs/.vimrc .'
-ssh $1 'cd && ln -s cfgs/.gitconfig .'
-ssh $1 'cd && ln -s cfgs/.profile .'
+ssh $1 'cd && ln -sf cfgs/.vim .'
+ssh $1 'cd && ln -sf cfgs/.vimrc .'
+ssh $1 'cd && ln -sf cfgs/.gitconfig .'
+ssh $1 'cd && ln -sf cfgs/.profile .'
